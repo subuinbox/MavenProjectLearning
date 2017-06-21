e0edfbce49421801b37221e53520b0473fc5c72a
subuinbox <subuinbox@rediffmail.com>
2017-06-21 17:17:37 +0530

    2nd update

--- TestProject/src/main/java/Maven/MavenProjectLearning/Sample.java
+++ TestProject/src/main/java/Maven/MavenProjectLearning/Sample.java
@@ -8,6 +8,6 @@ public class Sample
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World after update!" );
+        System.out.println( "Hello World after update2!" );
     }
 }
