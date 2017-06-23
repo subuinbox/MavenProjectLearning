f40bfb7ebdd44801394d3fe5962a704706bee1e3
subuinbox <subuinbox@rediffmail.com>
2017-06-23 23:53:27 +0530

    chumma

--- TestProject/src/main/java/Maven/MavenProjectLearning/App.java
+++ TestProject/src/main/java/Maven/MavenProjectLearning/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.err.println("to learn github update correction");
+        System.err.println("to learn github update correctio");
     }
 }
