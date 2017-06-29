63238b50ecc3c1008536fe7b4257f912456629df
subuinbox <subuinbox@rediffmail.com>
2017-06-29 21:59:19 +0530

    learn with ganesh

--- TestProject/src/main/java/Maven/MavenProjectLearning/Sample.java
+++ TestProject/src/main/java/Maven/MavenProjectLearning/Sample.java
@@ -9,6 +9,6 @@ public class Sample
     public static void main( String[] args )
     {
         System.out.println( "Hello World after update2!" );
-        System.out.println("Learnig with Anand ");
+        System.out.println("Learnig with Ganesh ");
     }
 }
